38712f37c670b756d4dc1fab59ac66eeba8571e6
Tarvincoder <lavintarvin@gmail.com>
2026-09-22 16:36:46 +0300

    Clean up ticket.java by removing unused code
    Removed commented-out code for while and do-while loops related to ticket sales.

--- ticket.java
+++ ticket.java
@@ -19,33 +19,6 @@ public class Ticketingdiscount {
 		for(int a=1;a<=10;a++ )
 		System.out.println("Ticket 00"+a);
 		
-		
-		//while loop
-		//used when you don't know how many times something will actually repeat
-		// for example selling tickets until the finish
-		
-		int availableTickets=50;
-		int takenTickets=10;
-		while(availableTickets >=takenTickets) {
-			availableTickets=availableTickets-takenTickets;
-			System.out.println("Sold 10 tickets.Remaining:"+availableTickets);
-		}
-		System.out.println("Tickets sold out!");
-		
-		//do while loop
-		//it runs at least once even if the condition is false
-		//for example asking a customer to enter a ticket number
-		int availabletickets=50;
-		int tickets;
-		do {
-			System.out.println("Enter the number of tickets that you want");
-			tickets=15;
-			System.out.println("You have entered:"+tickets);
-		}while (tickets>availabletickets);
-		
-		
-
-		
 		}
 
 	}
