6587af2b061e5c76151eaf44b289a5422c521965
Joyritah-256 <joyritahnankya@gmail.com>
2026-09-22 16:04:08 +0300

    Using for loop to add discount for the first 10 members

---
+++ assignmnetoop/src/assignmnetoop/Ticketingdiscount.java
@@ -0,0 +1,28 @@
+package assignmnetoop;
+
+public class Ticketingdiscount {
+
+
+		public static void main(String[] args) {
+			
+			
+		
+		//for loop
+		//used when you know the exact number of times you want something to happen
+		// and now we want to offer this discount for only  the first 10 members
+		// Nankya Joy Ritah 2500715516 25/u/15516/ps
+		for(int a=1;a<=10;a++ )
+		System.out.println("Ticket 00"+a);
+		
+		
+		
+		
+				
+		
+
+		
+		}
+
+	}
+
+
